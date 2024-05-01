bdd067819df17db0f943867c244ec1fbb697754b
skskdmsdl <skskdmsdl@gmail.com>
2024-05-01 22:42:58 +0900

    practice streamlit

--- Home.py
+++ Home.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from langchain.prompts import PromptTemplate
+from datetime import datetime
+
+today = datetime.today().strftime("%H:%M:%S")
+
+st.title(today)
 
 st.title("Hello world!")
 
@@ -15,4 +21,8 @@ st.write("hello")
 st.write([1, 2, 3, 4, 5])
 
 st.write({"x": 1})
+
+st.write(PromptTemplate)
+
+st.selectbox("Choose your model", ("GPT-3", "GPT-4"))
 
