0a469551a00bdd25d6354040759c39dd9a5ec8e1
skskdmsdl <skskdmsdl@gmail.com>
2024-05-03 23:01:03 +0900

    add pages

--- Home.py
+++ Home.py
@@ -2,6 +2,11 @@ import streamlit as st
 from langchain.prompts import PromptTemplate
 from datetime import datetime
 
+st.set_page_config(
+    page_title="FullstackGPT",
+    page_icon="🧸",
+)
+
 today = datetime.today().strftime("%H:%M:%S")
 
 st.title(today)
@@ -26,5 +31,17 @@ st.write(PromptTemplate)
 
 st.selectbox("Choose your model", ("GPT-3", "GPT-4"))
 
-st.sidebar.title("sidebar title")
+with st.sidebar:
+    st.title("sidebar title")
+    st.text_input("extra")
 
+tab_one, tab_two, tab_three = st.tabs(["A", "B", "C"])
+
+with tab_one:
+    st.write("a")
+
+with tab_two:
+    st.write("b")
+
+with tab_three:
+    st.write("c")
