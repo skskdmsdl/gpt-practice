2a8ca0ed2cdff4af1d48aa91a4024ece35c4f480
skskdmsdl <skskdmsdl@gmail.com>
2024-05-04 22:46:49 +0900

    code 정리

--- Home.py
+++ Home.py
@@ -9,28 +9,6 @@ st.set_page_config(
 
 today = datetime.today().strftime("%H:%M:%S")
 
-st.title(today)
-
-st.title("Hello world!")
-
-st.subheader("Welcome to Streamlit")
-
-st.markdown(
-    """
-    #### I Love it
-    """
-)
-
-st.write("hello")
-
-st.write([1, 2, 3, 4, 5])
-
-st.write({"x": 1})
-
-st.write(PromptTemplate)
-
-st.selectbox("Choose your model", ("GPT-3", "GPT-4"))
-
 with st.sidebar:
     st.title("sidebar title")
     st.text_input("extra")
