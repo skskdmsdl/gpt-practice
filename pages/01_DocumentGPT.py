0a3b9e5776781fc752d8e9e449c969c671b2c999
skskdmsdl <skskdmsdl@gmail.com>
2024-05-05 23:45:32 +0900

    study chat message

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,3 +1,11 @@
 import streamlit as st
 
 st.title("Document GTP")
+
+with st.chat_message("human"):
+    st.write("Hello")
+
+with st.chat_message("ai"):
+    st.write("how are you")
+
+st.chat_input("Send a message to the ai")
