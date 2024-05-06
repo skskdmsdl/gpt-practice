34e4242c450a6e027721abc29d60ae365bde1629
skskdmsdl <skskdmsdl@gmail.com>
2024-05-06 23:03:42 +0900

    study chat message

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -1,3 +1,4 @@
+import time
 import streamlit as st
 
 st.title("Document GTP")
@@ -7,5 +8,9 @@ with st.chat_message("human"):
 
 with st.chat_message("ai"):
     st.write("how are you")
+
+with st.status("Embedding file..."):
+    time.sleep(3)
+    st.write("Getting a file download...")
 
 st.chat_input("Send a message to the ai")
