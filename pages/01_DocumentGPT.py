a2ea4759dbbdbf38960f3a8ac836a3f46faccbba
skskdmsdl <skskdmsdl@gmail.com>
2024-05-07 22:55:44 +0900

    write 확장시키기

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -9,8 +9,12 @@ with st.chat_message("human"):
 with st.chat_message("ai"):
     st.write("how are you")
 
+st.chat_input("Send a message to the ai")
+
 with st.status("Embedding file..."):
     time.sleep(3)
-    st.write("Getting a file download...")
-
-st.chat_input("Send a message to the ai")
+    st.write("Getting the file")
+    time.sleep(3)
+    st.write("Embedding the file")
+    time.sleep(3)
+    st.write("Caching the file")
