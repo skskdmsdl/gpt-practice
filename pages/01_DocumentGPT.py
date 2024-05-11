5390826fc909a7d44c5e9678b92232b8401ffb55
skskdmsdl <skskdmsdl@gmail.com>
2024-05-11 22:36:16 +0900

    save message

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -11,12 +11,14 @@ st.title("Document GTP")
 if "messages" not in st.session_state:
     st.session_state["messages"] = []
 
-st.write(st.session_state["messages"])
-
-def send_message(message, role):
+def send_message(message, role, save=True):
     with st.chat_message(role):
         st.write(message)
+    if save:
         st.session_state["messages"].append({"message":message, "role":role})
+
+for message in st.session_state["messages"]:
+    send_message(message["message"], message["role"], save=False)
 
 message = st.chat_input("Send a message to the ai")
 
